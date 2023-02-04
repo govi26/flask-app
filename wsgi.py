aa666eb5e46199954e8f0b54fac0f29fd3d931b3
Govind Yadav <govindyadav.suny@gmail.com>
2023-02-03 21:46:55 -0500

    Fixing path.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.insert(0, '/var/www/html/flask-app')
+sys.path.insert(0, '/var/www/html/simple_flask_app')
 
-from flaskapp import app as application
+from simple_flask_app import app as application
